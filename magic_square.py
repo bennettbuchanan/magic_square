b3ca4446d7b5712ea0383af993bc4e082e65f889
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-08-02 21:52:53 -0700

    Add working magic square checker.

--- magic_square.py
+++ magic_square.py
@@ -1,42 +1,51 @@
-"""Create the array of coordinate tuples."""
-coord = [(x, y) for x in range(3) for y in range(3)]
 
-"""The array for a false magic 3x3 square."""
-false = range(9)
+def check_magic(n, vals):
+    """Determine if a square of size n is magic.
+
+    Keyword arguments:
+    n -- The dimension of the square.
+    vals -- The coordinates of the square and the value at that coordinate.
+    """
+    list = [[], [], [], [], []]
+
+    for i in range(n):
+        for v in vals:
+            if v[0][0] == i:
+                list[0].append(v)
+            if v[0][1] == i:
+                list[1].append(v)
+            if v[0][0] == i and v[0][1] == i:
+                list[2].append(v)
+            if v[0][0] == i and v[0][1] == (n - 1) - i:
+                list[3].append(v)
+
+        list[4].append(sum([list[0][i][1] for i in range(n)]))
+        list[4].append(sum([list[1][i][1] for i in range(n)]))
+    list[4].append(sum([list[2][i][1] for i in range(n)]))
+    list[4].append(sum([list[3][i][1] for i in range(n)]))
+
+    return check_equality(list[4])
+
+
+def check_equality(list):
+    """Check that all integers in a list are equal.
+
+    Keyword arguments:
+    list -- A list of integers.
+    """
+    for i in list:
+        if i != list[0]:
+            return False
+
+    return True
+
+"""Dimension of square."""
+n = 3
+
+"""Create the array of coordinate tuples."""
+coord = [(x, y) for x in range(n) for y in range(n)]
 
 """The array for a proper magic 3x3 square."""
 magic = [8, 1, 6, 3, 5, 7, 4, 9, 2]
 
-"""Add the associated values to the tuple coords."""
-vals = zip(coord, magic)
-
-
-def add_items(list):
-    """Adds items in a series."""
-    sum = 0
-    for i in range(len(list)):
-        sum += list[i][1]
-    return sum
-
-
-def get_rows(vals, j):
-    """Slice the list to get the rows and columns."""
-    return [vals[i] for i in range(j)]
-
-
-def check_series(vals):
-    """Check that the values are equal."""
-    list = []
-    for i in range(3):
-        for j in vals:
-            if j[0][0] == i:
-                list.append(j)
-                prev = add_items(list)
-                """Add function that gets the sum of the next series for
-                comparison.
-                """
-                if prev != add_items(list):
-                    return False
-    return True
-
-print check_series(vals)
+print check_magic(n, zip(coord, magic))

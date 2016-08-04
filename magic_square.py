62e2817cfdd28dbaef56d7bcf2d2a07d9cc03445
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-08-04 07:23:40 -0700

    Remove extraneous variables.

--- magic_square.py
+++ magic_square.py
@@ -11,7 +11,7 @@ def check_magic(matrix):
 
     cross = [[], []]
 
-    check = 0
+    check = sum(matrix[0])
 
     for i in range(len(matrix)):
         matrix_col = []
@@ -21,27 +21,27 @@ def check_magic(matrix):
             cross[0].append(matrix[i][i])
             cross[1].append(matrix[(len(matrix) - 1) - i][i])
 
-            if sum(matrix[0]) != sum(row):
+            if check != sum(row):
                 return False
-        if sum(matrix[0]) != sum(row) or sum(matrix[0]) != sum(matrix_col):
+        if check != sum(row) or check != sum(matrix_col):
             return False
-    if sum(matrix[0]) != sum(set(cross[0])) or sum(matrix[0]) != sum(set(cross[1])):
+    if check != sum(set(cross[0])) or check != sum(set(cross[1])):
         return False
     return True
 
 """Uncomment line below to test magic 3x3 square checker."""
-# print check_magic([[4, 3, 8], [9, 5, 1], [2, 7, 6]])
+# print check_magic([[2, 7, 6], [9, 5, 1], [4, 3, 8]])
 
 
-def recursive(slice1, slice2, place, next, slice):
+def recursive(slice1, slice2, next, slice):
     tmp = list(slice2)
     new = range(len(slice2))
     n = slice2.pop(next)
-    new[place] = n
+    new[0] = n
     magic_set = []
 
-    for i in range(place, len(slice2)):
-        new[place + (i + 1)] = slice2[i]
+    for i in range(0, len(slice2)):
+        new[0 + (i + 1)] = slice2[i]
 
     if next == len(tmp) - 1:
         list_to_matrix = []
@@ -56,26 +56,26 @@ def recursive(slice1, slice2, place, next, slice):
         return
 
     if slice < 9:
-        brute_force_magic(slice1 + new, 0, 0, slice + 1)
-    if recursive(slice1, tmp, place, next + 1, slice):
+        brute_force_magic(slice1 + new, 0, slice + 1)
+    if recursive(slice1, tmp, next + 1, slice):
         return True
 
 
-def brute_force_magic(curr, place, next, slice):
+def brute_force_magic(curr, next, slice):
     tmp = list(curr)
     new = range(len(curr))
     n = curr.pop(next)
-    new[place] = n
+    new[0] = n
 
-    for i in range(place, len(curr)):
-        new[place + (i + 1)] = curr[i]
+    for i in range(0, len(curr)):
+        new[0 + (i + 1)] = curr[i]
 
     if next == len(tmp) - 1:
-        recursive(new[:slice], new[slice:], 0, 0, slice)
+        recursive(new[:slice], new[slice:], 0, slice)
         return
 
-    recursive(new[:slice], new[slice:], 0, 0, slice)
-    brute_force_magic(tmp, place, next + 1, slice)
+    recursive(new[:slice], new[slice:], 0, slice)
+    brute_force_magic(tmp, next + 1, slice)
 
 
-print brute_force_magic([1, 2, 3, 4, 5, 6, 7, 8, 9], 0, 0, 1)
+print brute_force_magic([1, 2, 3, 4, 5, 6, 7, 8, 9], 0, 1)
